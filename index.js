560f37638fe3305ada3d2e3007c3efb1a3abc35c
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2020-09-27 00:28:26 -0500

    trying to sync changes

---
+++ index.js
@@ -0,0 +1,20 @@
+function animation() {
+  let tl = gsap.timeline({ repeat: 0 });
+
+  // tl.to(".landing-page", {
+  //   backgroundColor: " #84dd05",
+  //   borderRadius: "15%",
+  //   width: "400px",
+  //   height: "400px",
+  //   duration: 2,
+  //   border: "3px solid green",
+  // });
+  tl.set(".shamwow", { x: 700, y: 100 });
+  tl.to(".shamwow", {
+   
+    x: 700,
+    scale: 1.6,
+    filter: "brightness: 1.2",
+  });
+}
+animation();
