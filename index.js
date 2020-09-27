08be863c36b649f57375e29a0e2f0324a7a017e9
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2020-09-27 02:10:57 -0500

    tentative animations.

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 function animation() {
   let tl = gsap.timeline({ repeat: 0 });
-
-  // tl.to(".landing-page", {
-  //   backgroundColor: " #84dd05",
-  //   borderRadius: "15%",
-  //   width: "400px",
-  //   height: "400px",
-  //   duration: 2,
-  //   border: "3px solid green",
-  // });
-  tl.set(".h2", { x: 700, y: 100 });
-  tl.to(".h2", {
-   
-    x: 700,
-    scale: 1.6,
-    filter: "brightness: 1.2",
-  });
+  let tl2 = gsap.timeline({ repeat: 0 });
+  
+  
+  tl2.set(".hellyeah", { x: 100, y: 20,  });
+  tl2.to(".hellyeah", { x: 50, y: 300, rotation: 360, width: "35%", duration: 2.5, delay: 1 })
 }
 animation();
