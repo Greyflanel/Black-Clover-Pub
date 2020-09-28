774a8c3ebd6e16c07654f43194a46c1b20d9f1c2
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2020-09-28 18:50:44 -0500

    working on clover animation.

--- index.js
+++ index.js
@@ -1,17 +1,82 @@
 function animation() {
   let tl = gsap.timeline({ repeat: 0 });
+  let tl1 = gsap.timeline({ repeat: 0 });
   let tl2 = gsap.timeline({ repeat: 0 });
-  
-  
-  
-  tl.set(".logo", { x: "10%", y: "50%",  transform: "rotate(-25deg)" });
-  tl2.set(".clover", { x: "222%", y: "-92%", width: "16%", transform: "rotate(-26deg)", filter: "blur(0.11em)" });
+  let tl3 = gsap.timeline({ repeat: 0 });
+  let tl4 = gsap.timeline({ repeat: 0 });
 
-  tl2.to(".clover", {
-    x: "168%", y: "-50%", width: "35%", duration: 2, delay: 2, filter: "blur(0)"
-  })
+  tl.set(".logo", { x: "25%", y: "50%", transform: "rotate(-25deg)" });
+  tl1.set(".clover1", {
+    display: "inline",
+    x: "271%",
+    y: "-93%",
+    width: "16%",
+    transform: "rotate(-26deg)",
+    filter: "blur(0.11em)",
+  });
 
+  tl2.set(".clover2", {
+    display: "inline",
+    x: "271%",
+    y: "-93%",
+    width: "16%",
+    transform: "rotate(-26deg)",
+    filter: "blur(0.11em)",
+  });
+
+  tl3.set(".clover3", {
+    display: "inline",
+    x: "271%",
+    y: "-93%",
+    width: "16%",
+    transform: "rotate(-26deg)",
+    filter: "blur(0.11em)",
+  });
+
+  tl4.set(".clover4", {
+    display: "inline",
+    x: "271%",
+    y: "-93%",
+    width: "16%",
+    transform: "rotate(-26deg)",
+    filter: "blur(0.11em)",
+  });
+
+  tl1.to(".clover1", {
+    delay: 1.2,
+    display: "inline",
+    x: "380%",
+    y: "-100%",
+    width: "18%",
+    filter: "blur(0)",
+  });
+  
+  tl2.to(".clover2", {
+    delay: 1.2,
+    display: "inline",
+    x: "20%",
+    y: "-110%",
+    width: "18%",
+    filter: "blur(0)",
+  });
+
+  tl3.to(".clover3", {
+    delay: 1.2,
+    display: "inline",
+    x: "400%",
+    y: "28%",
+    width: "18%",
+    filter: "blur(0)",
+  });
+
+  tl4.to(".clover4", {
+    delay: 1.2,
+    display: "inline",
+    x: "250%",
+    y: "32%",
+    width: "18%",
+    filter: "blur(0)",
+  });
 }
-
 
 animation();
