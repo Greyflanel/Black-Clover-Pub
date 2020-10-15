97f3a5929c6503cd2571cc74dc996ff8b967f9dc
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2020-10-15 01:08:09 -0500

    testing

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ function mediaqueryresponse(mql) {
     });
     tlMediaQueryText.to(".clover1-text, .clover2-text, .clover3-text, .clover4-text", {
       fontSize: "0.73em",
-      width: "300%"
+     
     });
     
   } else if (mqls[1].matches) {
