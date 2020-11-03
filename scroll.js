56631accd17b66676b9ab140b38d91fd0c23d293
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2020-11-03 17:47:36 -0600

    completed navbar text recoloring on scroll

--- scroll.js
+++ scroll.js
@@ -2,48 +2,47 @@
 
 gsap.registerPlugin(ScrollTrigger);
 let timeline = gsap.timeline({
-  scrollTrigger: ".content",
+  scrollTrigger: ".home-container",
 });
 
 
 function scrollAnimation() {
   
-  gsap.to(".nav", {
-    color: "red",
+  gsap.to(".navbar-links a", {
+    color: "yellow",
+    filter: "brightness(300%)",
     scrollTrigger: {
-      trigger: "#live-music",
-      scroller: ".container",
+      trigger: "#home",
+      scroller: ".home-container",
       scrub: true,
       ease: "power1-in",
-      start: "top 25%", //when top  passes the bottom viewport height
-      end: "bottom 5%", //when bottom of herman passes the bottom viewport height
+      start: "bottom 25%", //when top  passes the bottom viewport height
+      end: "center 100%", //when bottom of herman passes the bottom viewport height
 
       //events: onEnter onLeave onEnterBack onLeaveBack
       toggleActions: "restart complete reverse reset",
       //options: play, pause, resume, reset, restart, complete, reverse,none
     },
   });
-  gsap.to("nav", {
-   
-    // ease: "power1-in",
+  gsap.to(".navbar-links a", {
+    color: "white",
     scrollTrigger: {
       trigger: "#gallery",
-      scroller: ".container",
+      scroller: ".home-container",
       scrub: true,
       start: "top 25%",
-      end: "bottom 5%",
+      end: "bottom top",
       toggleActions: "restart complete reverse reset",
     },
   });
-  gsap.to(".nav", {
-    
-    backgroundColor: "rgb(18, 22, 0.5)",
+  gsap.to(".navbar", {
+    backgroundColor: "rgba(0, 0, 0, 1)",
     ease: "power1-out",
     scrollTrigger: {
       trigger: "#contact",
-      scroller: ".container",
+      scroller: ".home-container",
       scrub: true,
-      start: "top center",
+      start: "top 25%",
       end: "bottom 5%",
       toggleActions: "restart complete reverse reset",
     },
@@ -52,18 +51,7 @@ function scrollAnimation() {
     rotation: 45,
     
   });
-  // gsap.to(".three-d-clover", {
-  //   y: "2rem",
-  //   scrollTrigger: {
-  //     trigger: "#contact",
-  //     scroller: ".container",
-  //     scrub: 4,
-  //     start: "top top",
-  //     end: "bottom bottom",
-  //     toggleActions: "restart complete reverse reset",
-  //   },
-  // });
-    
+  
 }
 
 scrollAnimation();
