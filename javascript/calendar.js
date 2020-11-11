7e6ad3223c483fc5bfe39a689d76c7a4345e1952
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2020-11-10 20:46:33 -0600

    testing

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var calendarEl = document.getElementById("calendar");
   var calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: "dayGridMonth",
-    googleCalendarApiKey: "",
+    googleCalendarApiKey: "AIzaSyCdPcedSFOEqBmQe-4lTQEvMkn7Is9a5Ig",
     events: {
       googleCalendarId: "richardlovelace811@gmail.com",
     },
