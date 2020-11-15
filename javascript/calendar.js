42b88791121d4b550d5150f62960035b01b32e5a
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2020-11-15 01:13:06 -0600

    Im not sure about color change, but it needed something different

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
   var calendarEl = document.getElementById("calendar");
   var calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: "listMonth",
-    googleCalendarApiKey: "",
+    googleCalendarApiKey: "AIzaSyCdPcedSFOEqBmQe-4lTQEvMkn7Is9a5Ig",
     events: {
-      googleCalendarId: "",
+      googleCalendarId: "richardlovelace811@gmail.com",
     },
   });
   
