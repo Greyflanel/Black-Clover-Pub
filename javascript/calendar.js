5219ac7ae3da01da6f4b52296ab24037b4c3f72d
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2020-11-23 01:10:07 -0600

    removing

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
   var calendarEl = document.getElementById("calendar");
   var calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: "listMonth",
-    googleCalendarApiKey: "AIzaSyCdPcedSFOEqBmQe-4lTQEvMkn7Is9a5Ig",
+    googleCalendarApiKey: "",
     events: {
-      googleCalendarId: "richardlovelace811@gmail.com",
+      googleCalendarId: "",
     },
   });
   
