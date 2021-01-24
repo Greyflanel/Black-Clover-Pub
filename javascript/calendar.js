cc10dd19b4a2b385097ee80234750cf37c73bed1
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2021-01-23 20:34:34 -0600

    testing calendar

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
   var calendarEl = document.getElementById("calendar");
   var calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: "listMonth",
-    googleCalendarApiKey: "",
+    googleCalendarApiKey: "AIzaSyDKWIW9XrOIv2jfjA1Ok0fUo8JOdrXD5v4",
     events: {
-      googleCalendarId: "",
+      googleCalendarId: "blackcloverbar@gmail.com",
     },
   });
   
